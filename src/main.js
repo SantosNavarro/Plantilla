25f59fd6e7ad4ba10faa2552f5dd35dd5d420ec0
Santos Navarro <773541@alu.murciaeduca.es>
2024-01-07 21:05:31 +0100

    1
    b

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log("Hola");
+
+
+
+
+
+
+const { readFileSync } = require('fs')
+
+const posthtml = require('posthtml')
+const include = require('posthtml-include')
+
+const html = readFileSync('index.html')
+
+posthtml([ include({ encoding: 'utf8' }) ])
+    .process(html)
+    .then((result) => console.log(result.html))
