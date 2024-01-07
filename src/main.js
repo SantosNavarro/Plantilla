91faa70059bd2963e0ca5bd1c7b8fa830f38ad23
Santos Navarro <773541@alu.murciaeduca.es>
2024-01-07 23:50:31 +0100

    c
    3

--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
+import "@fortawesome/fontawesome-free/js/all";
 
 
 
 
-
-
-const { readFileSync } = require('fs')
-
-const posthtml = require('posthtml')
-const include = require('posthtml-include')
-
-const html = readFileSync('index.html')
-
-posthtml([ include({ encoding: 'utf8' }) ])
-    .process(html)
-    .then((result) => console.log(result.html))
